b4af2cf9c3e96b1927c25cc6f60520982b90bc08
Harlyn Luna <91364909+HJosueLB@users.noreply.github.com>
2023-10-29 15:31:50 -0600

    Prueba 1 - Se inicializa proyecti

---
+++ src/main/java/com/mycompany/testingproject_githubnetbeans/TestingProject_GitHubNetbeans.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testingproject_githubnetbeans;
+
+/**
+ *
+ * @author harly
+ */
+public class TestingProject_GitHubNetbeans {
+
+    public static void main(String[] args) {
+        System.out.println("Prueba para testear");
+    }
+}
